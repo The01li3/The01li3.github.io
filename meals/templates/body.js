53e81e1c96bff3cb4df9c8f2d0d8e79a6b143d67
The01li3 <ollie.p.m@hotmail.com>
2024-07-16 16:40:55 +0100

    major changes

--- meals/templates/body.js
+++ meals/templates/body.js
@@ -1,71 +1,18 @@
-header = '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>';
-header += `
-<div class="row justify-content-center">
-    <div id="logo" class="col-1"><p>T</p>        
+header = `
+<div class="row g-0 col-8 justify-content-center">
+    <div class="row col-9 flex-nowrap justify-content-start">
+        <div id="logo" class="col col-1">
+            <p class="text-center">T</p>        
+        </div>
+        <div id="headwrap" class="col d-none d-xxl-block col-7">
+            <p>The01li3's Recipes</p>
+        </div>
     </div>
-    <div id="headwrap" class="col-4">
-        <p>The01li3's Recipes</p>
-    </div>
+    <div id="search-div" class="col-3 justify-content-end my-auto">
+        <form id="search-inline" class="input-group">
+            <input type="search" id="site-search" class="form-control form-control-sm" placeholder="Search"/>
+            <button type="submit" class="btn btn-outline-search"><i class="bi bi-search"></i></button>            
+        </form>
+    </div>    
 </div>`
 document.getElementById('headerBanner').innerHTML = header;
-
-vtext = `<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-<div>gdfsgdfgfd</div>
-
-`
-
-                document.getElementById('imhere').innerHTML = vtext;
--- meals/templates/meta.js
+++ meals/templates/meta.js
@@ -1,4 +1,5 @@
 header = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">'
+header += `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">`;
 
 header +=`<meta name="description" content="The01li3's Recipes">
 		<meta name="keywords" content="">
