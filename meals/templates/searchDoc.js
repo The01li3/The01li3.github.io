f982b31f871948e7b1aad573f96fa154762e4a5a
The01li3 <ollie.p.m@hotmail.com>
2024-07-23 16:54:46 +0100

    search doc populated

--- meals/templates/searchDoc.js
+++ meals/templates/searchDoc.js
@@ -1,18 +1,292 @@
 var varrRecipes = [
 	{
-		"id":"crepePancakes.html",
-		"title":"Pancakes",
-		"description":" pancake, dessert, vegetarian",
-		"ingredients":["milk", "plain flour", "sugar", "egg", "oil", "lemon", "butter"],
-		"utensils":"",
-	},
-	{
 		"id":"americanPancakes.html",
 		"title":"American Pancakes",
-		"description":" American, pancake, dessert, vegetarian",
+		"description":"American, pancake, dessert, vegetarian",
 		"ingredients":["milk", "plain flour", "baking powder", "sugar", "egg", "oil", "butter", "salt", "pecans", "maple syrup"],
-		"utensils":"cast iron",
-	}];
+		"utensils":"cast iron"
+	},
+	{
+		"id":"bananaCake",
+		"title":"Banana Cake",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Loose Bottom Cake Tin"
+	},
+	{
+		"id":"buffaloCauliflowerCasserole",
+		"title":"Buffalo Cauliflower Casserole",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Casserole Dish"
+	},
+	{
+		"id":"buffaloRanchPasta",
+		"title":"Buffalo Ranch Pasta",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"buffaloSauce",
+		"title":"Buffalo Sauce",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"carrotCake",
+		"title":"Carrot Cake",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Loose Bottom Cake Tin"
+	},
+	{
+		"id":"castIronNaan",
+		"title":"Cast Iron Naan",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Cast Iron"
+	},
+	{
+		"id":"chanaMasala",
+		"title":"Chana Masala",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Slow Cooker"
+	},
+	{
+		"id":"cheeseAndMarmiteSwirls",
+		"title":"Cheese And Marmite Swirls",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"chickenAlfredo",
+		"title":"Chicken Alfredo",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"coconutRice",
+		"title":"Coconut Rice",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"creamCheeseFrosting",
+		"title":"Cream Cheese Frosting",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"crepePancakes.html",
+		"title":"Pancakes",
+		"description":"pancake, dessert, vegetarian",
+		"ingredients":["milk", "plain flour", "sugar", "egg", "oil", "lemon", "butter"],
+		"utensils":""
+	}
+	{
+		"id":"garlicAndGingerCurry",
+		"title":"Garlic And Ginger Curry",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"greekStyleBeans",
+		"title":"Greek Style Beans",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Casserole Dish"
+	},
+	{
+		"id":"hoisinCashewTofu",
+		"title":"Hoisin Cashew Tofu",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"kasespatzle",
+		"title":"Kasespatzle",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"lemonPosset",
+		"title":"Lemon Posset",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Ramekin"
+	},
+	{
+		"id":"lemonStirFry",
+		"title":"Lemon Stir Fry",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"meadBasics",
+		"title":"Mead Basics",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"…TBD"
+	},
+	{
+		"id":"misoSoup",
+		"title":"Miso Soup",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"nachoCasserole",
+		"title":"Nacho Casserole",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Casserole Dish"
+	},
+	{
+		"id":"noKneadFocaccia",
+		"title":"No Knead Focaccia",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Cast Iron"
+	},
+	{
+		"id":"pecanTassies",
+		"title":"Pecan Tassies",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Mini Muffin Tray, Stamper, Rolling Pin"
+	},
+	{
+		"id":"pineappleColeslaw",
+		"title":"Pineapple Coleslaw",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"ramenBase",
+		"title":"Ramen Base",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"risottoBasics",
+		"title":"Risotto Basics",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"satay",
+		"title":"Satay",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"slowCookedButterChicken",
+		"title":"Slow Cooked Butter Chicken",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Slow Cooker"
+	},
+	{
+		"id":"slowCookedVegetarianRedThaiCurry",
+		"title":"Slow Cooked Vegetarian Red Thai Curry",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Slow Cooker"
+	},
+	{
+		"id":"spicedAppleCake",
+		"title":"Spiced Apple Cake",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Square Cake Tin"
+	},
+	{
+		"id":"spinacheRicottaCannelloni",
+		"title":"Spinache Ricotta Cannelloni",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Piping Bag"
+	},
+	{
+		"id":"springRolls",
+		"title":"Spring Rolls",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"stickyLemonTofu",
+		"title":"Sticky Lemon Tofu",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"stickyMangoRice",
+		"title":"Sticky Mango Rice",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"sushiBasics",
+		"title":"Sushi Basics",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":"Rolling Mat"
+	},
+	{
+		"id":"tamarindFishCurry",
+		"title":"Tamarind Fish Curry",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"tempehTacos",
+		"title":"Tempeh Tacos",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"tikkaMasalaSalmon",
+		"title":"Tikka Masala Salmon",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"tiramisu",
+		"title":"Tiramisu",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	},
+	{
+		"id":"uniBaguette",
+		"title":"Uni Baguette",
+		"description":"1,2",
+		"ingredients":["1", "2", "3", "4", "5", "6", "7"],
+		"utensils":""
+	}
+];
 
 var vRecipesIndex = lunr(function () {
 	this.ref("id")
