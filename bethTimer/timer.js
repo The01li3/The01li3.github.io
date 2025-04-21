618987a43ff81e12c4aacdb401d18f273b233c3a
The01li3 <ollie.p.m@hotmail.com>
2025-04-21 13:15:45 +0100

    TimerSetup

--- bethTimer/timer.js
+++ bethTimer/timer.js
@@ -14,3 +14,40 @@ function widget_addLoadEvent(pFunction)
         }
     }
 };
+
+var vCount = 0;
+var vSeconds = 0;
+var vTimer = document.getElementById("timer");
+var vBtnStart = document.getElementById('btn-start');
+var vBtnStop = document.getElementById('btn-pause');
+var vBtnReset = document.getElementById('btn-refresh');
+
+vBtnStart.addEventListener("click", function () {
+    timer = true;
+    vBtnStart.disabled = true; 
+    vBtnStop.disabled = false;
+    startTimer();
+});
+
+vBtnStop.addEventListener("click", function() {
+    vBtnStart.disabled = false;
+    vBtnStop.disabled = true;
+    timer = false;
+});
+
+vBtnReset.addEventListener("click", function() {
+    vTimer.innerHTML = "00";
+    vBtnStart.disabled = false;
+    vBtnStop.disabled = true;
+    timer = false;
+    vSeconds = 0;
+});
+
+
+function startTimer() {
+    if(timer){
+        vSeconds++;
+        vTimer.innerHTML = vSeconds;
+        setTimeout(startTimer,1000);
+    }
+}
